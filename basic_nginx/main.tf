e6c8a96e9836e9d1ac062390b478d1d36eb1f7c8
Andy Mender <andymenderunix@gmail.com>
2025-12-06 21:18:37 +0100

    Add notes on the docker provider

--- basic_nginx/main.tf
+++ basic_nginx/main.tf
@@ -1,4 +1,9 @@
-# NOTE: Needed to initialize Docker?
+# NOTE: Direct provider features include:
+# - Running Docker on a remote 'host' (via SSH with optional 'ssh_opts', or TCP + TLS)
+# - Pulling Docker images from a private registry via 'registry_auth'
+#   and an associated Terraform 'data' block called "docker_registry_image"
+#   (credentials can be handled via a separate config file or a credentials helper tool)
+# - Passing a custom config via a "docker_service" 'resource' block
 provider "docker" {}
 
 # NOTE: 'random_pet' is a reserved Terraform resource name, 'petname' is just a field label
@@ -8,9 +13,16 @@ resource "random_pet" "petname" {
   separator = "-"
 }
 
+data "docker_registry_image" "nginx" {
+  name = "nginx:1.29"
+}
+
 resource "docker_image" "nginx" {
-  name         = "nginx:1.29"
-  keep_locally = false
+  name         = data.docker_registry_image.nginx.name
+  # NOTE: Allows tracking container content changes as opposed to just tracking the image tag
+  pull_triggers = [data.docker_registry_image.nginx.sha256_digest]
+  # NOTE: To avoid keeping the base image locally - useful in bare metal deployments
+  # keep_locally = false
 }
 
 # TODO: Use a dedicated name for each project
