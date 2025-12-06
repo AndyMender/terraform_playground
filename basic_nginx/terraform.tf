2b6b40847de4ce8e0734a09c5e9630566af26ecf
Andy Mender <andymenderunix@gmail.com>
2025-12-06 19:20:11 +0100

    Add extra notes about providers to terraform tf

--- basic_nginx/terraform.tf
+++ basic_nginx/terraform.tf
@@ -1,6 +1,9 @@
+# NOTE: Central (root) config defining which components (providers) and which versions are required
+# NOTE2: New providers can be authored and submitted by OSS contributors
 terraform {
   required_providers {
     docker = {
+      # NOTE: Shorthand provider names will default to 'registry.terraform.io' to fully resolve
       source  = "kreuzwerker/docker"
       # NOTE: Allow patch version updates, but no changes to major and minor versions via '~>'
       # ('~>' applies to right-most version specified)
