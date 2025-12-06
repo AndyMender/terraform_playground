49a126b4e6165bc6d6d16ac14ace37bb2ca3d095
Andy Mender <andymenderunix@gmail.com>
2025-12-06 19:22:07 +0100

    Bump provider versions in basic_nginx

--- basic_nginx/terraform.tf
+++ basic_nginx/terraform.tf
@@ -7,13 +7,13 @@ terraform {
       source  = "kreuzwerker/docker"
       # NOTE: Allow patch version updates, but no changes to major and minor versions via '~>'
       # ('~>' applies to right-most version specified)
-      version = "~> 3.0.2"
+      version = "~> 3.6.2"
     }
   
     random = {
       source  = "hashicorp/random"
       # NOTE: Multiple ','-separated version conditions are allowed
-      version = "~> 3.1.0"
+      version = "~> 3.7.2"
     }
   }
   # NOTE: Versions 1.x are allowed. Below is set as the lowest compatible version.
