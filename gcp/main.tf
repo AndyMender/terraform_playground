09843bdc348df0bee735c67d8a4a31f1010c8b20
Andy Mender <andymenderunix@gmail.com>
2025-12-12 12:47:38 +0100

    Add zone to google provider, name network resource custom name

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,9 +1,10 @@
 provider "google" {
   project     = "mender-gcp-demo"
   region      = "eu-west1"
+  zone        = "eu-west1-a"
 }
 
-resource "google_compute_network" "mender-vpc" {
+resource "google_compute_network" "mender_vpc" {
   name = "mender-vpc"
 }
 
